a6d47e52a9396f21778a11771f2ab275dd549d5c
Kenneth-Javier <kj.ortigoza@gmail.com>
2021-01-25 11:43:05 +0100

    Some clean writings

--- CinemaDramaticSolutions/Program.cs
+++ CinemaDramaticSolutions/Program.cs
@@ -9,18 +9,18 @@ namespace CinemaDramaticSolutions
         static void Main(string[] args)
         {
             //1.
-            welcomeClient();
+            PrintWelcomeClient();
             //2.
-            inputFromClient();
+            InputFromClient();
             
         }
 
         //0,2.Skapa skalet till en Switch-sats som till en början har Två Cases.
         //Ett för ”0” somstänger ner programmet och ett default som berättar att det är felaktig input.
-        private static void inputFromClient()
+        private static void InputFromClient()
         {
             bool m = true;
-            printMenu();
+            PrintMenu();
             //0,3.Skapa en ​oändlig iteration, ​alltså något som inte tar slut innan vi säger till att den ska ta slut. 
             //Detta löser ni med att skapa en egen bool med tillhörande while-loop.
             do
@@ -32,29 +32,34 @@ namespace CinemaDramaticSolutions
                 {
                     //klar bugg string catch faliure
                     case "1":
-                        Console.WriteLine("Menyval 1: Ungdom eller pensionär");
-                        ticketPrice(askpersonage());
+                        StrOut("Menyval 1: Ungdom eller pensionär");
+                        TicketPrice(AskPersonAge());
+                        StrOut("\n");
+                        PrintMenu();
                         break;
                     //klar
                     case "2":
-                        Console.WriteLine("Menyval 2: Upprepa tio gånger");
-                        Console.WriteLine("Skriv det du vill ska upprepas tio gånger:");
-                        String tenT = Console.ReadLine();
-                        repeat10times(tenT);
+                        StrOut("Menyval 2: Upprepa tio gånger");
+                        StrOut("Skriv det du vill ska upprepas tio gånger:");
+                        String tenTimesStr = Console.ReadLine();
+                        Repeat10times(tenTimesStr);
+                        PrintMenu();
                         break;
                     //klar
                     case "3":
-                        Console.WriteLine("Menyval 3: Det tredje ordet");
-                        Console.WriteLine("Skriv en mening med minst tre ord");
-                         //= "glökjdslkgj sdgdsga TRE agag";
+                        StrOut("Menyval 3: Det tredje ordet");
+                        StrOut("Skriv en mening med minst tre ord");
                         var words = Console.ReadLine();
-                        toStringSpl(words);
+                        ToStrSplit(words);
+                        StrOut("\n");
+                        PrintMenu(); 
                         break;
                     //klar
                     case "4":
                         Console.WriteLine("Menyval 4: Räkna ut priset för ett sällskap");
-                        String text = "Ange antal biljetter till sälskapet? (ange endast i siffror)";
-                        menyval4(text);
+                        string str = "Ange antal biljetter till sälskapet? (ange endast i siffror)";
+                        Menyval4(str);
+                        PrintMenu();
                         break;
                         //klar
                     case "0":
@@ -70,7 +75,7 @@ namespace CinemaDramaticSolutions
             } while (m);
         }
 
-        private static void toStringSpl(string words)
+        private static void ToStrSplit(string words)
         {
             var w = words.Split();
             var first = w[1];
@@ -78,27 +83,21 @@ namespace CinemaDramaticSolutions
             Console.WriteLine(w3);
         }
 
-        private static void repeat10times(String text)
+        private static void Repeat10times(string str)
         {
             for (int i = 0; i < 10; i++)
             {
-                Console.Write((i + 1) + ". " + text + ", ");
+                Console.Write((i + 1) + ". " + str + ", ");
             }
-            
-        }
-
-        private static string askpersonage()
-        {
-            return "Ange ålder i siffror";
+            StrOut("\n");
         }
 
         //Menyval 1: Ungdom eller pensionär
-        //
-        private static int ticketPrice(String text)
+        private static int TicketPrice(string str)
         {
             //metod behöver:
             int ticketprice;
-            int number = stringtoint(text);
+            int number = Stringtoint(str);
             if ((number < 20) && (number > 4))
             {
                 ticketprice = 80;
@@ -127,13 +126,13 @@ namespace CinemaDramaticSolutions
             return ticketprice;
         }
 
-        private static int stringtoint(string text)
+        private static int Stringtoint(string text)
         {
             bool success = false;
             int number;
             do
             {
-                strOut(text);
+                StrOut(text);
                 string x = Console.ReadLine();
                 success = false;
                 success = int.TryParse(x, out number);
@@ -143,15 +142,11 @@ namespace CinemaDramaticSolutions
             return number;
         }
 
-        private static void strOut(String text)
-        {
-            Console.WriteLine(text);
-        }
 
         //bör använda sig av menyval1() samt priceCounter() för att skriva ut totala priset
-        private static void menyval4(String text)
+        private static void Menyval4(string text)
         {
-            int antalbiljetter = stringtoint(text);
+            int antalbiljetter = Stringtoint(text);
             bool success = false;
             int totalprice =0;
             int i = 0;
@@ -161,36 +156,67 @@ namespace CinemaDramaticSolutions
                 while (!success)
                 {
                     i++;
-                    totalprice += ticketPrice(askpersonage());
+                    totalprice += TicketPrice(AskPersonAge());
                     if (i == antalbiljetter)
                     {
                         success = true;
                         i = 0;
                     }
                 }
-
-                Console.WriteLine("Du Skall alltså ha " + antalbiljetter + " biljetter till sälskapet för " + totalprice + "kr");
+                StrOut("\n" + "Du Skall alltså ha " + antalbiljetter + " biljetter till sälskapet för totalt " + totalprice + "kr" + "\n");
             }
         }
 
-        private static void printMenu()
+        private static void StrOut(string str)
         {
-            Console.WriteLine("Huvudmeny: ");
-            Console.WriteLine("Menyval 1: Ungdom eller pensionär");
-            Console.WriteLine("Menyval 2: Upprepa tio gånger");
-            Console.WriteLine("Menyval 3: Det tredje ordet");
-            Console.WriteLine("Menyval 4: Räkna ut priset för ett sällskap");
-            Console.WriteLine("Menyval 0 för att avsluta program");
+            Console.WriteLine(str);
         }
+
+        private static string AskPersonAge()
+        {
+            return "Ange ålder i siffror";
+        }
+
 
         //0,1.Berätta för användaren att de har kommit till huvudmenyn och de kommernavigera genom att skriva in siffror för att testa olika funktioner.
-        private static void welcomeClient()
+        private static void PrintWelcomeClient()
         {
-            Console.WriteLine("Welcome to the Cinema!");
-            Console.WriteLine("Du har kommit till huvudmenyn och kommer navigera genom att skriva in siffror för att testa olika funktioner.");
-            Console.WriteLine("Tryck Enter för att fortsätta.");
-            var x = Console.ReadLine();
+            StrOut("Welcome to the Cinema!");
+            StrOut("Du har kommit till huvudmenyn och kommer navigera genom att skriva in siffror för att testa olika funktioner.");
+            StrOut("Tryck sedan Enter för att fortsätta.");
+            StrOut("\n");
         }
 
+        private static void PrintMenu()
+        {
+
+            StrOut("Huvudmeny: ");
+            StrOut("Menyval 1: Ungdom eller pensionär");
+            StrOut("Menyval 2: Upprepa tio gånger");
+            StrOut("Menyval 3: Det tredje ordet");
+            StrOut("Menyval 4: Räkna ut priset för ett sällskap");
+            StrOut("Menyval 0 för att avsluta program");
+            StrOut("\n");
+        }
+
+    }
+    namespace StringApplication
+    {
+
+        class StringProg
+        {
+
+            static void SMain(string[] args)
+            {
+                string[] starray = new string[]{"Down the way nights are dark",
+            "And the sun shines daily on the mountain top",
+            "I took a trip on a sailing ship",
+            "And when I reached Jamaica",
+            "I made a stop"};
+
+                string str = String.Join("\n", starray);
+                Console.WriteLine(str);
+            }
+        }
     }
 }
