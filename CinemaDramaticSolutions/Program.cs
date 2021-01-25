7b202badc9acd1cc2da2e512c5cae9671f564449
Kenneth-Javier <kj.ortigoza@gmail.com>
2021-01-25 12:56:49 +0100

    Comented methods done

--- CinemaDramaticSolutions/Program.cs
+++ CinemaDramaticSolutions/Program.cs
@@ -5,94 +5,81 @@ namespace CinemaDramaticSolutions
 {
     class Program
     {
-        //From git
         static void Main(string[] args)
         {
             //1.
             PrintWelcomeClient();
             //2.
-            InputFromClient();
+            HandleOptions();
             
         }
 
-        //0,2.Skapa skalet till en Switch-sats som till en början har Två Cases.
-        //Ett för ”0” somstänger ner programmet och ett default som berättar att det är felaktig input.
-        private static void InputFromClient()
+        //Handles the menu optoions for the program
+        private static void HandleOptions()
         {
-            bool m = true;
-            PrintMenu();
-            //0,3.Skapa en ​oändlig iteration, ​alltså något som inte tar slut innan vi säger till att den ska ta slut. 
-            //Detta löser ni med att skapa en egen bool med tillhörande while-loop.
+            //While true, menu will be running
+            bool menuActual = true;
             do
             {
+                //prints out menu
+                PrintMenu();
+                //switch recives input from user and handle options chose dependent of users string input
                 string x = Console.ReadLine();
                 string caseSwitch = x;
-                //0,4.Bygg ut menyn med val att exekvera de övriga övningarna.
                 switch (caseSwitch)
                 {
-                    //klar bugg string catch faliure
                     case "1":
                         StrOut("Menyval 1: Ungdom eller pensionär");
                         TicketPrice(AskPersonAge());
                         StrOut("\n");
-                        PrintMenu();
                         break;
-                    //klar
                     case "2":
-                        StrOut("Menyval 2: Upprepa tio gånger");
-                        StrOut("Skriv det du vill ska upprepas tio gånger:");
-                        String tenTimesStr = Console.ReadLine();
+                        StrOut("Menyval 2: Upprepa tio gånger" + "\n" + "Skriv det du vill ska upprepas tio gånger:");
+                        string tenTimesStr = Console.ReadLine();
                         Repeat10times(tenTimesStr);
-                        PrintMenu();
                         break;
-                    //klar
                     case "3":
-                        StrOut("Menyval 3: Det tredje ordet");
-                        StrOut("Skriv en mening med minst tre ord");
+                        StrOut("Menyval 3: Det tredje ordet" + "\n" + "Skriv en mening med minst tre ord");
                         var words = Console.ReadLine();
                         ToStrSplit(words);
                         StrOut("\n");
-                        PrintMenu(); 
                         break;
-                    //klar
                     case "4":
-                        Console.WriteLine("Menyval 4: Räkna ut priset för ett sällskap");
+                        StrOut("Menyval 4: Räkna ut priset för ett sällskap");
                         string str = "Ange antal biljetter till sälskapet? (ange endast i siffror)";
-                        Menyval4(str);
-                        PrintMenu();
+                        CompanyAmoutAndCost(str);
                         break;
-                        //klar
                     case "0":
-                        Console.WriteLine("Menyval 0 för att avsluta program");
-                        m = false;
+                        StrOut("Menyval 0 för att avsluta program");
+                        menuActual = false;
                         Environment.Exit(0);
                         break;
-                        //klar
                     default:
-                        Console.WriteLine("Felaktig input, försök igen enligt anvisningarna:");
+                        StrOut("Felaktig input, försök igen enligt anvisningarna:");
                         break;
                 }
-            } while (m);
+            } while (menuActual);
         }
 
+        //Splitts string content in to var(string) array, gets the thid string from w array and writes out to user
         private static void ToStrSplit(string words)
         {
             var w = words.Split();
-            var first = w[1];
             var w3 = w[2];
-            Console.WriteLine(w3);
+            StrOut(w3);
         }
 
+        //Outprints typed word 10 times with number of times before
         private static void Repeat10times(string str)
         {
             for (int i = 0; i < 10; i++)
             {
-                Console.Write((i + 1) + ". " + str + ", ");
+                StrOut((i + 1) + ". " + str + ", ");
             }
             StrOut("\n");
         }
 
-        //Menyval 1: Ungdom eller pensionär
+        //calculates all individual tickets cost
         private static int TicketPrice(string str)
         {
             //metod behöver:
@@ -126,6 +113,7 @@ namespace CinemaDramaticSolutions
             return ticketprice;
         }
 
+        //converts strings to ints
         private static int Stringtoint(string text)
         {
             bool success = false;
@@ -142,9 +130,8 @@ namespace CinemaDramaticSolutions
             return number;
         }
 
-
-        //bör använda sig av menyval1() samt priceCounter() för att skriva ut totala priset
-        private static void Menyval4(string text)
+        //calculates how all tickets cost in total
+        private static void CompanyAmoutAndCost(string text)
         {
             int antalbiljetter = Stringtoint(text);
             bool success = false;
@@ -167,56 +154,41 @@ namespace CinemaDramaticSolutions
             }
         }
 
+        //Prints out strings
         private static void StrOut(string str)
         {
             Console.WriteLine(str);
         }
 
+        //asks persons age
         private static string AskPersonAge()
         {
             return "Ange ålder i siffror";
         }
 
-
-        //0,1.Berätta för användaren att de har kommit till huvudmenyn och de kommernavigera genom att skriva in siffror för att testa olika funktioner.
+        //Prints out how to use the menu system
         private static void PrintWelcomeClient()
         {
-            StrOut("Welcome to the Cinema!");
-            StrOut("Du har kommit till huvudmenyn och kommer navigera genom att skriva in siffror för att testa olika funktioner.");
-            StrOut("Tryck sedan Enter för att fortsätta.");
-            StrOut("\n");
+            string[] starray = new string[] { "Welcome to the Cinema!",
+            "Du har kommit till huvudmenyn och kommer navigera genom att skriva in siffror för att testa olika funktioner.",
+            "Tryck sedan Enter för att fortsätta.",
+            "\n" };
+            string str = String.Join("\n", starray);
+            StrOut(str);
         }
 
+        //Prints out the menu
         private static void PrintMenu()
         {
-
-            StrOut("Huvudmeny: ");
-            StrOut("Menyval 1: Ungdom eller pensionär");
-            StrOut("Menyval 2: Upprepa tio gånger");
-            StrOut("Menyval 3: Det tredje ordet");
-            StrOut("Menyval 4: Räkna ut priset för ett sällskap");
-            StrOut("Menyval 0 för att avsluta program");
-            StrOut("\n");
-        }
-
-    }
-    namespace StringApplication
-    {
-
-        class StringProg
-        {
-
-            static void SMain(string[] args)
-            {
-                string[] starray = new string[]{"Down the way nights are dark",
-            "And the sun shines daily on the mountain top",
-            "I took a trip on a sailing ship",
-            "And when I reached Jamaica",
-            "I made a stop"};
-
-                string str = String.Join("\n", starray);
-                Console.WriteLine(str);
-            }
+            string[] starray = new string[]{"Huvudmeny: ",
+            "Menyval 1: Ungdom eller pensionär",
+            "Menyval 2: Upprepa tio gånger",
+            "Menyval 3: Det tredje ordet",
+            "Menyval 4: Räkna ut priset för ett sällskap",
+            "Menyval 0 för att avsluta program",
+            "\n" };
+            string str = String.Join("\n", starray);
+            StrOut(str);
         }
     }
 }
